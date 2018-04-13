2d503dcffd8e5f482a18f6f83bfb63ed4e6f2ca2
Madan Kv <kmadan@live.in>
2018-04-13 13:56:37 +0530

    Update AppTest.java

--- src/test/java/com/qaagility/controller/AppTest.java
+++ src/test/java/com/qaagility/controller/AppTest.java
@@ -2,7 +2,7 @@ package com.qaagility.controller;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import com.qaagility.controller.Calculator;
+import com.qaagility.javaee.Calculator;
  
 
 public class AppTest {
