dc8af1b34a3bc26392ce0d1633599212108300c8
VivekRajSingh <vivekrajsingh4u@gmail.com>
2019-03-11 18:28:13 +0530

    Update test

--- src/test/java/com/qaagility/controller/CalcTest.java
+++ src/test/java/com/qaagility/controller/CalcTest.java
@@ -28,7 +28,8 @@ public class CalcTest {
       }
 	
         @Test
-      public void testCaladd = new Calculator();
-
+      public void testCaladd = new Calculator()
+      {
+         
 		assertEquals(testCalAdd.add(),8); 	
     }
